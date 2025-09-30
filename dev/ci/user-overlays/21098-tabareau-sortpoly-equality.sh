a752fe66981e5f42c7cd499068715efff35f88ff
nicolas tabareau <nicolas.tabareau@gmail.com>
2025-09-30 11:44:58 +0200

    start fixing ci

---
+++ dev/ci/user-overlays/21098-tabareau-sortpoly-equality.sh
@@ -0,0 +1,7 @@
+overlay hott https://github.com/tabareau/HoTT sortpoly-equality 21098
+
+overlay unimath https://github.com/tabareau/UniMath sortpoly-equality 21098
+
+overlay category_theory https://github.com/tabareau/category-theory sortpoly-equality 21098
+
+overlay metarocq https://github.com/tabareau/metarocq sortpoly-equality 21098
